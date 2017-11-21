af365ff94e0bcc47983f4ef3131b4282fa0544e8
victor.pose.contractor <victor.pose.contractor@bbva.com>
2017-11-21 11:25:55 +0100

    more modal

--- todo-list/src/components/ui/Modal/index.jsx
+++ todo-list/src/components/ui/Modal/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getModal } from 'redux/modal/reducer';
 import { closeModal } from 'redux/modal/actions';
+import keycode from 'keycode';
 
 import './styles.css';
 import RemoveTaskDialog from './RemoveTaskDialog';
@@ -12,6 +13,22 @@ const MODAL_COMPONENTES = {
 }
 
 class Modal extends Component {
+
+  closeModal= (event) => {
+    if (keycode(event) === 'esc'){
+      this.props.closeModal();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.child === null && nextProps.child) {
+      document.addEventListener('keydown', this.closeModal);
+    }
+    if (this.props.child && nextProps.child === null) {
+      document.addEventListener('keydown', this.closeModal);
+    }
+  }
+
   render() {
     const { child, childProps } = this.props;
     const ModalChild = MODAL_COMPONENTES[child] || null;
